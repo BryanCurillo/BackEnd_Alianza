3bdc805a899efbc26b7f84e67c340e2cce57b4c4
Bryan Curillo <bryan.curillo.est@tecazuay.edu.ec>
2023-08-17 00:18:47 -0500

    paquetes y repository (Bryan)

---
+++ alianza/src/main/java/com/backend/alianza/repository/FichaFamiliarRepository.java
@@ -0,0 +1,7 @@
+package com.backend.alianza.repository;
+
+import com.backend.alianza.model.FichaFamiliar;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FichaFamiliarRepository extends JpaRepository<FichaFamiliar, Long> {
+}
