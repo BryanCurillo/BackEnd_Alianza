21cf82175b09ea5c81168198abfad6dbaecf0a51
BryanCurillo <bryan.curillo.est@tecazuay.edu.ec>
2023-09-04 23:02:50 -0500

    nombre del url de ficha personal corregido

--- alianza/src/main/java/com/backend/alianza/controller/FichaPersonalController.java
+++ alianza/src/main/java/com/backend/alianza/controller/FichaPersonalController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/tipoPersonal")
+@RequestMapping("/fichaPersonal")
 public class FichaPersonalController {
 
     @Autowired
