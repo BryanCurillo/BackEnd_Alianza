3825488fef2220f0ea38aea704290ac865974ef4
BryanCurillo <bryan.curillo.est@tecazuay.edu.ec>
2023-09-12 02:13:59 -0500

    data.sql

---
+++ alianza/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO rol(id_rol, descripcion_rol, nombre_rol)
+VALUES (1, '', 'Docente');
+INSERT INTO rol(id_rol, descripcion_rol, nombre_rol)
+VALUES (2, '', 'Secretaria');
+INSERT INTO rol(id_rol, descripcion_rol, nombre_rol)
+VALUES (3, '', 'Encargado');
