688d1adadd5fca5112304f71c513470dce23c1ed
Alexis <mateosiavig@gmail.com>
2023-09-12 03:21:05 -0500

    Cambios para controller

---
+++ alianza/src/main/java/com/backend/alianza/repository/PersonaRepository.java
@@ -0,0 +1,7 @@
+package com.backend.alianza.repository;
+
+import com.backend.alianza.model.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonaRepository extends JpaRepository<Persona,Long> {
+}
